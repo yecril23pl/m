3e5e3fbf52425afa95a23bcfa7a3ccb7cd62b471
yecril23pl <151100823+yecril23pl@users.noreply.github.com>
2023-12-14 06:37:54 +0100

    Fix mistakes

--- do-it.c
+++ do-it.c
@@ -13,20 +13,20 @@ else s += (++ d << 01) - 01; return np;
 }
 int const main (int const, char const *const [])
 {
-enum MaxTry { N = 023 }; char n = {};
+enum MaxTry { N = 023 }; char n [01] = {};
 fprintf
   (stderr,
   "Counting numbers (n) up to (%d) that divide ((n 1 – +)!)…\n",
   N);
-for (; n ++ < N; )
+for (; * n ++ < N; )
   {
-    char l_f [N] = {}, char f = {};
-    fprintf (stderr, "(%d):", n);
-    factorise (n, l_f);
-    for (; f < N; ++ f)
+    char l_f [N] = {}, f [01] = {};
+    fprintf (stderr, "(%d):", * n);
+    factorise (* n, l_f);
+    for (; * f < N; ++ f)
       {
-        char e = p [f];
-        for (; e --; ) fprintf (stderr, " * %d", f + 01);
+        char e = l_f [* f];
+        for (; e --; ) fprintf (stderr, " * %d", * f + 01);
       }
     fputc ('\n', stderr);
   }
