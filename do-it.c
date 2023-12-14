d37709baf1f439ab2a90219f775d4e55bfacf89c
yecril23pl <151100823+yecril23pl@users.noreply.github.com>
2023-12-14 06:31:15 +0100

    Factorise

--- do-it.c
+++ do-it.c
@@ -1,7 +1,33 @@
 #include<stdlib.h>
 #include<stdio.h>
+static int const factorise (int np,  char fp [])
+{
+char d = 02; short s = 04;
+for (; s < np;)
+{
+div_t const q = div (np, d);
+if (! q .rem) {
+  np = q .quot;
+++ fp [d - 01]; }
+else s += (++ d << 01) - 01; return np;
+}
 int const main (int const, char const *const [])
 {
-enum MaxTry { N = 023 };
-fprintf (stderr, "Counting numbers (n) up to %d that divide ((n 1 – +)!)…", N);
+enum MaxTry { N = 023 }; char n = {};
+fprintf
+  (stderr,
+  "Counting numbers (n) up to (%d) that divide ((n 1 – +)!)…\n",
+  N);
+for (; n ++ < N; )
+  {
+    char l_f [N] = {}, char f = {};
+    fprintf (stderr, "(%d):", n);
+    factorise (n, l_f);
+    for (; f < N; ++ f)
+      {
+        char e = p [f];
+        for (; e --; ) fprintf (stderr, " * %d", f + 01);
+      }
+    fputc ('\n', stderr);
+  }
   return EXIT_FAILURE; }
