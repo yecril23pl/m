6387f49fbbfc3141e87d6a8fac2b1ef271004393
yecril23pl <151100823+yecril23pl@users.noreply.github.com>
2023-12-14 04:38:32 +0100

    Progress report

--- do-it.c
+++ do-it.c
@@ -1,3 +1,7 @@
 #include<stdlib.h>
+#include<stdio.h>
 int const main (int const, char const *const [])
-{ return EXIT_FAILURE; }
+{
+enum MaxTry { N = 023 };
+fprintf (stderr, "Counting numbers (n) up to %d that divide ((n 1 – +)!)…", N);
+  return EXIT_FAILURE; }
