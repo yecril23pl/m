57d25ff4a0170b4330b727536e472e6b594567a1
yecril23pl <151100823+yecril23pl@users.noreply.github.com>
2023-12-14 06:42:36 +0100

    Fix mistakes

--- do-it.c
+++ do-it.c
@@ -9,7 +9,8 @@ div_t const q = div (np, d);
 if (! q .rem) {
   np = q .quot;
 ++ fp [d - 01]; }
-else s += (++ d << 01) - 01; return np;
+else s += (++ d << 01) - 01; }
+  return np;
 }
 int const main (int const, char const *const [])
 {
@@ -18,12 +19,12 @@ fprintf
   (stderr,
   "Counting numbers (n) up to (%d) that divide ((n 1 – +)!)…\n",
   N);
-for (; * n ++ < N; )
+for (; (* n) ++ < N; )
   {
     char l_f [N] = {}, f [01] = {};
     fprintf (stderr, "(%d):", * n);
     factorise (* n, l_f);
-    for (; * f < N; ++ f)
+    for (; * f < N; ++ * f)
       {
         char e = l_f [* f];
         for (; e --; ) fprintf (stderr, " * %d", * f + 01);
