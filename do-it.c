8b193cdd74f79ed713a1ef46b1e53e95a5a91244
yecril23pl <151100823+yecril23pl@users.noreply.github.com>
2023-12-18 00:44:49 +0100

    check divisibility of factors

--- do-it.c
+++ do-it.c
@@ -1,8 +1,12 @@
 #include<stdlib.h>
 #include<stdio.h>
-static int const factorise (int np,  char fp [])
+static int const factorise (int /* the number to factorise */
+np,  char /* (fp [i]) will be the greatest power of (i + 1) that divides the parameter (np) */
+fp [])
 {
-char d = 02; short s = 04;
+char /* divisor */
+  d = 02; short /* square of the divisor (d) */
+  s = 04;
 for (; s <= np;)
 {
 div_t const q = div (np, d);
@@ -12,6 +16,11 @@ if (! q .rem) {
 else s += (++ d << 01) - 01; } ++ fp [np - 01];
   return np;
 }
+
+static int const fill_id (char /* output buffer, to be filled with consecutive integers */
+l_n [], int const /* length of the output buffer */
+n) { for (int x = 0; x < n; ++ x) l_n [x] = x; return n; }
+
 int const main (int const, char const *const [])
 {
 enum MaxTry { N = 023 }; char n [01] = {};
@@ -21,13 +30,20 @@ fprintf
   N);
 for (; (* n) ++ < N; )
   {
-    char l_f [N] = {}, f [01] = {};
+    char /* factors of (* n) */
+    l_f [N] = {}, /* factor index */
+    f [01] = {}, /* previous integers */
+    l_n [N]
+    ; fill_id
+      (l_n, * n);
     fprintf (stderr, "(%d):", * n);
     factorise (* n, l_f);
     for (; * f < N; ++ * f)
       {
-        char e = l_f [* f];
-        for (; e --; ) fprintf (stderr, " * %d", * f + 01);
+        char e = l_f [* f], x = *f;
+        ;
+        for (; e --; ) { div_t const q = div (l_n [x], *f); if (! q .rem) l_n [x] = q .quot;
+          fprintf (stderr, " * %d %d", * f + 01, q .rem); }
       }
     fputc ('\n', stderr);
   }
